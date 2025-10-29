5d2c7031de500727f31b4693ba6b0953311b23be
Ryan Licata <rlicata34@gmail.com>
2025-10-28 22:37:08 -0400

    add routes for home and shop

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
+import homeImage from "../../assets/home-image.jpg";
 import "./Home.css";
 
 function Home() {
-  return <section></section>;
+  return (
+    <section className="home">
+      <h1 className="home__title">Welcome to my Site</h1>
+      <img src={homeImage} alt="Home Image" className="home__image" />
+      <Link to="/shop" className="home__link">
+        <button className="home__button" type="button">
+          Shop Products
+        </button>
+      </Link>
+    </section>
+  );
 }
 
 export default Home;
